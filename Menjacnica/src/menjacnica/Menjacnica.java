b04aa95bc19f1543a7430448fe2f42e2e174b075
djordjeJeremic <djetic994@gmail.com>
2015-04-01 21:11:38 +0200

    Dodate equals,codehash i toString metode u obe klase

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -18,6 +18,41 @@ public class Menjacnica {
 	public void setOznaka(String oznaka) {
 		this.oznaka = oznaka;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((oznaka == null) ? 0 : oznaka.hashCode());
+		result = prime * result + ((valuta == null) ? 0 : valuta.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Menjacnica other = (Menjacnica) obj;
+		if (oznaka == null) {
+			if (other.oznaka != null)
+				return false;
+		} else if (!oznaka.equals(other.oznaka))
+			return false;
+		if (valuta == null) {
+			if (other.valuta != null)
+				return false;
+		} else if (!valuta.equals(other.valuta))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Menjacnica [valuta=" + valuta + ", oznaka=" + oznaka + "]";
+	}
+	
+	
 	
 
 }
