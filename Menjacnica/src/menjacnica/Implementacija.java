260e5aff0a99f6eca6b1b606da46354550aab0b8
djordjeJeremic <djetic994@gmail.com>
2015-04-01 21:50:48 +0200

    Dodata klasa Implementacija

---
+++ Menjacnica/src/menjacnica/Implementacija.java
@@ -0,0 +1,29 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfejs.Interfejs;
+
+public class Implementacija implements Interfejs {
+
+
+	public void dodajKurs(double prodajni, double kupovni, double srednji,
+			GregorianCalendar datum) {
+	
+		
+	}
+
+	
+	public Kurs obrisiKurs(GregorianCalendar datum) {
+	
+		return null;
+	}
+
+	@Override
+	public Kurs pronadnjiKurs(GregorianCalendar datum) {
+		
+		return null;
+	}
+	
+
+}
