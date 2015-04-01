10db5fd5a9e7d027615406626043b3062134d88e
djordjeJeremic <djetic994@gmail.com>
2015-04-01 21:09:56 +0200

    Napravljene klase Kurs i Menjacnica sa odgovarajucim atributima koje imaju svoje get i set metode

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,23 @@
+package menjacnica;
+
+public class Menjacnica {
+	
+	private String valuta;
+	private String oznaka;
+	
+	
+	public String getValuta() {
+		return valuta;
+	}
+	public void setValuta(String valuta) {
+		this.valuta = valuta;
+	}
+	public String getOznaka() {
+		return oznaka;
+	}
+	public void setOznaka(String oznaka) {
+		this.oznaka = oznaka;
+	}
+	
+
+}
